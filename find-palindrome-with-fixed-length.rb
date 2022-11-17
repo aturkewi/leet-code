f92dcd28b899826dd57046632240aebb0f87680c
Avi Turkewitz <aviturkewitz@gmail.com>
2022-11-17 11:59:51 -0500

    make palindrome solution class based to memoize

--- find-palindrome-with-fixed-length.rb
+++ find-palindrome-with-fixed-length.rb
@@ -14,6 +14,62 @@ def kth_palindrome(queries, int_length)
   end
 end
 
+class Solver
+  attr_reader :queries, :int_length
+
+  def initialize(queries, int_length)
+    @queries = queries
+    @int_length = int_length
+  end
+
+  def execute
+    puts "Starting number: #{starting_number}"
+    queries.map do |query|
+        if valid?(query)
+            first_half = starting_number + (query - 1)
+            generate_number(first_half)
+        else
+            -1
+        end
+    end
+  end
+
+  private
+
+  def valid?(query)
+    max_num >= (starting_number + (query - 1))
+  end
+
+  def max_num
+    # fancier way is to get the log or something
+    # power = Math.log(starting_number).to_i
+    # max_num = (10 ** (power + 1)) - 1
+
+    @max_num ||= starting_number.to_s.gsub("1", "9").gsub("0", "9").to_i
+  end
+  
+  def starting_number
+    @starting_number ||= set_starting_number
+  end
+
+  def set_starting_number
+    zero_count = (int_length - 1) / 2
+    starting_number = 1 * 10 ** zero_count
+  end
+  
+  def generate_number(first_half)
+    if int_length == 1
+        first_half
+    elsif int_length.odd?
+        left = first_half / 10
+        pivot = first_half % 10
+        (left.to_s + pivot.to_s + left.to_s.reverse).to_i
+    else
+        (first_half.to_s + first_half.to_s.reverse).to_i
+    end
+  end
+end
+
 def valid?(query, starting_number)
   # fancier way is to get the log or something
   max_num = starting_number.to_s.gsub("1", "9").gsub("0", "9").to_i
