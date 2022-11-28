e8fe3ae41b9d330d8273ad005e1e791852b2935b
Avi Turkewitz <aviturkewitz@gmail.com>
2022-11-27 22:26:03 -0500

    uncomment out good tests

--- exercises/validParens.test.js
+++ exercises/validParens.test.js
@@ -1,17 +1,17 @@
 const { isValid } = require('./validParens')
 
-// test('returns true for ()', () => {
-//   expect(isValid('()')).toEqual(true)
-// })
+test('returns true for ()', () => {
+  expect(isValid('()')).toEqual(true)
+})
 
 test('returns true for ()[]{}', () => {
   expect(isValid('()[]{}')).toEqual(true)
 })
 
-// test('returns true for empty string', () => {
-//   expect(isValid('')).toEqual(true)
-// })
+test('returns true for empty string', () => {
+  expect(isValid('')).toEqual(true)
+})
 
-// test('returns false for mismatch', () => {
-//   expect(isValid('(]')).toEqual(false)
-// })
+test('returns false for mismatch', () => {
+  expect(isValid('(]')).toEqual(false)
+})
