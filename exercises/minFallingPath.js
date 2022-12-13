1d880ea2bd553346fdfd6f8d1338cbf8c3857de9
Avi Turkewitz <aviturkewitz@gmail.com>
2022-12-13 11:42:34 -0500

    memoize minSum for each point in matrix

--- exercises/minFallingPath.js
+++ exercises/minFallingPath.js
@@ -6,8 +6,9 @@
  */
 const minFallingPathSum = (matrix) => {
   let min = undefined
+  const shortcuts = {}
   for(let column=0; column < matrix[0].length; column++){
-    const sum = fallingPathSum(matrix, 0, column)
+    const sum = fallingPathSum(matrix, 0, column, shortcuts)
     if(min === undefined || sum < min){
       min = sum
     }
@@ -16,7 +17,7 @@ const minFallingPathSum = (matrix) => {
   return min
 };
 
-const fallingPathSum = (matrix, row, column) => {
+const fallingPathSum = (matrix, row, column, shortcuts) => {
   if(matrix[row] === undefined){
     return 0
   }
@@ -24,11 +25,16 @@ const fallingPathSum = (matrix, row, column) => {
   if(matrix[row][column] === undefined){
     return null
   }
-
-  const left = fallingPathSum(matrix, row + 1, column - 1)
-  const center = fallingPathSum(matrix, row + 1, column)
-  const right = fallingPathSum(matrix, row + 1, column + 1)
-  const minSum = getMin([left, center, right])
+  let minSum
+  if(shortcuts[`${row},${column}`]){
+    minSum = shortcuts[`${row},${column}`]
+  }else{
+    const left = fallingPathSum(matrix, row + 1, column - 1, shortcuts)
+    const center = fallingPathSum(matrix, row + 1, column, shortcuts)
+    const right = fallingPathSum(matrix, row + 1, column + 1, shortcuts)
+    minSum = getMin([left, center, right])
+    shortcuts[`${row},${column}`] = minSum
+  }
 
   return matrix[row][column] + minSum
 }
